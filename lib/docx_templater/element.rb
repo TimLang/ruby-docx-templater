a26a4708b1118c7a8ea06bab7f44ebf79280e7fa
tim <tim@jiaoxuebang.com>
2013-12-20 17:40:57 +0800

    added method create_table

--- lib/docx_templater/element.rb
+++ lib/docx_templater/element.rb
@@ -8,7 +8,7 @@ module DocxTemplater
     def create_paragraph element
       do_builder do |builder|
         xml = builder.tag!('w:p', {'w:rsidR' => '0019258A', 'w:rsidRDefault' => '00214805'}) do
-          builder.tag!('w:rPr'){builder.tag!('w:rPr'){builder.tag!('w:rFonts', {'w:hint' => 'eastAsia'})}}
+          builder.tag!('w:pPr'){builder.tag!('w:rPr'){builder.tag!('w:rFonts', {'w:hint' => 'eastAsia'})}}
           builder << element
         end
       end
@@ -26,6 +26,51 @@ module DocxTemplater
     def create_whilespace
       do_builder do |builder|
         builder.tag!('w:t', {'xml:space' => 'preserve'})
+      end
+    end
+
+    def create_table rows, width="2763"
+      return nil if !rows.is_a?(Array) || !rows[0].is_a?(Array)
+      do_builder do |builder|
+        builder.tag!('w:r') do
+          builder.tag!('w:rPr') do
+            builder.tag!('w:rFonts', {'w:hint' => 'eastAsia'})
+            builder.tag!('w:noProof')
+          end
+          builder.tag!('w:tbl') do
+            builder.tag!('w:tblPr') do
+              builder.tag!('w:tblStyle', {'w:val' => 'a5'})
+              builder.tag!('w:tblW', {'w:w' => '0', 'w:type' => 'auto'})
+              builder.tag!('w:tblLook', {'w:val' => '04A0'})
+            end
+            builder.tag!('w:tblGrid') do
+              rows.size.times do
+                builder.tag!('w:gridCol', {'w:w' => width})
+              end
+            end
+
+            rows.each do |row|
+              #TR
+              builder.tag!('w:tr', {'w:rsidR' => '006217C8', 'w:rsidTr' => '006217C8'}) do
+
+                row.each do |col|
+                  #TD
+                  builder.tag!('w:tc') do
+                    builder.tag!('w:tcPr'){builder.tag!('w:tcW', {'w:w' => width, 'w:type' => 'dxa'})}
+                    builder.tag!('w:p', {'w:rsidR' => '006217C8', 'w:rsidRDefault' => '006217C8'}) do
+                      builder.tag!('w:r') do
+                        builder.tag!('w:rPr'){builder.tag!('w:rFonts', {'w:hint' => 'eastAsia'})}
+                        builder.tag!('w:t') { builder << col.to_s}
+                      end
+                    end
+                  end
+                end
+
+              end
+            end
+            #
+          end
+        end
       end
     end
 
